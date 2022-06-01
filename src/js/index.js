5b4cf90626338264cba116fbf6a708f0781514a5
savage-viktor <viktor.dkn@gmail.com>
2022-06-01 10:27:43 +0300

    dev

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,64 @@
 import { fetchImages } from "./fetch"
 
+const galleryDiv = document.querySelector(".gallery")
+const formRef = document.querySelector(".search-form")
+
+formRef.addEventListener("submit", onSubmit)
+
 let request = "Cats"
 let page = 1
 
-fetchImages(request, page)
-    .then(renderGallery)
-    .catch(showError)
+// fetchImages(request, page)
+//     .then(renderGallery)
+//     .catch(showError)
+
 
 
 function renderGallery(res) {
     console.log(res.hits)
+
+    const cardsArray = []
+
     res.hits.map(image => {
         const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = image
-
-        console.log(webformatURL, largeImageURL, tags, likes, views, comments, downloads)
+        cardsArray.push(
+            `<div class="photo-card">
+                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+                <div class="info">
+                    <p class="info-item">
+                    <b>Likes: ${likes}</b>
+                    </p>
+                    <p class="info-item">
+                    <b>Views: ${views}</b>
+                    </p>
+                    <p class="info-item">
+                    <b>Comments: ${comments}</b>
+                    </p>
+                    <p class="info-item">
+                    <b>Downloads: ${downloads}</b>
+                    </p>
+                 </div>
+             </div>`
+        )
     })
+
+    galleryDiv.innerHTML = cardsArray.join("")
 }
 
 function showError(err) {
     console.log(err)
+}
+
+function onSubmit(ev) {
+
+    ev.preventDefault()
+
+    console.dir(ev.currentTarget.elements.searchQuery.value)
+
+    let request = ev.currentTarget.elements.searchQuery.value
+
+    fetchImages(request, page)
+        .then(renderGallery)
+        .catch(showError)
+
 }
